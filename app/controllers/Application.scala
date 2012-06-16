3489c32f58bda4ac63a38896bf44629188792155
Carl Harroch <carl@novoda.com>
2012-06-16 19:44:50 +0100

    adding social secrue

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,10 +4,16 @@ import play.api._
 import play.api.mvc._
 import securesocial.core.UserService
 
-object Application extends Controller {
-  
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+object Application extends Controller with securesocial.core.SecureSocial {
+
+  def index = SecuredAction() {
+    implicit request =>
+      Ok(views.html.index("Your new application is ready." + request.user))
   }
-  
+
+  def test = SecuredAction() {
+    implicit request =>
+      Ok(views.html.index("welcome@ " + request.user.displayName))
+  }
+
 }
