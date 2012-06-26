450c2309be9fbe9f47ad334144c369b565b991a2
Carl Harroch <carl@novoda.com>
2012-06-26 21:41:25 +0100

    adding 2.sql to see if it works with heroky

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,35 @@
+# Tasks account and tag
+
+DROP TABLE IF EXISTS account;
+DROP TABLE IF EXISTS tag;
+
+# --- !Ups
+
+CREATE TABLE account (
+    id SERIAL PRIMARY KEY,
+    email varchar(255) NOT NULL UNIQUE,
+    password varchar(255) NOT NULL,
+    username varchar(255) NOT NULL,
+    fullname varchar(255),
+
+    isAdmin BOOLEAN NOT NULL DEFAULT false,
+
+    createdAt timestamp,
+    modifiedAt timestamp
+);
+
+CREATE TABLE tag (
+    id SERIAL PRIMARY KEY,
+    uid varchar(255) NOT NULL UNIQUE,
+
+    accountId INT NOT NULL,
+    FOREIGN KEY (accountId) REFERENCES account(id),
+
+    createdAt timestamp,
+    modifiedAt timestamp
+);
+
+# --- !Downs
+
+DROP TABLE IF EXISTS account;
+DROP TABLE IF EXISTS tag;
